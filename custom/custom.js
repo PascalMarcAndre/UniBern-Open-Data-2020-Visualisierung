111787f99acdc9c53cac448599527f20fb74c1a8
saidalidev <said-ali1997@hotmail.com>
2020-04-28 16:11:52 +0200

    Added behaviour of markers

--- custom/custom.js
+++ custom/custom.js
@@ -48,35 +48,74 @@ function createLeafletMap() {
  * Requests and adds all stations from Libero to map
  * TODO: Make certain parts regarding D3 reusable (e.g. adding circles and lines etc.)
  */
+var clickedAll = false;
+var mapMarkers = []
 function showAllStationsLibero() {
-    d3.sparql(LINDAS_ENDPOINT, query_allStationsLibero()).then((data) => {
-        console.log(data); // TODO: Remove
+    if (!clickedAll) {
+        d3.sparql(LINDAS_ENDPOINT, query_allStationsLibero()).then((data) => {
+            console.log(data); // TODO: Remove
 
-        // TODO: Use D3 instead of Leaflet
-        data.forEach(station => {
-            const coords = station.Coord.replace("POINT(", "").replace(")", "").split(' ');
+            // TODO: Use D3 instead of Leaflet
+            data.forEach(station => {
+                const coords = station.Coord.replace("POINT(", "").replace(")", "").split(' ');
 
-            L.marker([coords[1], coords[0]]).addTo(map).bindPopup(station.Name);
+                marker = L.marker([coords[1], coords[0]])
+                marker.addTo(map).bindPopup(station.Name);
+                this.mapMarkers.push(marker);
+
+            });
+            clickedAll = true
+
         });
-    });
+    }
+    else {
+        for (var i = 0; i < this.mapMarkers.length; i++) {
+            map.removeLayer(this.mapMarkers[i]);
+
+        }
+        clickedAll = false
+
+    }
+
 }
 
 /**
  * Requests and adds 100 longest short distances to map
  * TODO: Make certain parts regarding D3 reusable (e.g. adding circles and lines etc.)
  */
+var clickedShortest = false;
+var mapElements = []
+
 function show100LongestShortDistances() {
-    d3.sparql(LINDAS_ENDPOINT, query_100longestShortDistances()).then((data) => {
-        console.log(data); // TODO: Remove
+    if (!clickedShortest) {
 
-        // TODO: Use D3 instead of Leaflet
-        data.forEach(route => {
-            const coords1 = route.loc1.replace("POINT(", "").replace(")", "").split(' ');
-            const coords2 = route.loc2.replace("POINT(", "").replace(")", "").split(' ');
+        d3.sparql(LINDAS_ENDPOINT, query_100longestShortDistances()).then((data) => {
+            console.log(data); // TODO: Remove
 
-            L.marker([coords1[1], coords1[0]]).addTo(map).bindPopup(route.stNam1);
-            L.marker([coords2[1], coords2[0]]).addTo(map).bindPopup(route.stNam2);
-            L.polyline([[coords1[1], coords1[0]], [coords2[1], coords2[0]]], {color: 'blue'}).addTo(map);
+            // TODO: Use D3 instead of Leaflet
+            data.forEach(route => {
+                const coords1 = route.loc1.replace("POINT(", "").replace(")", "").split(' ');
+                const coords2 = route.loc2.replace("POINT(", "").replace(")", "").split(' ');
+
+                marker1 = L.marker([coords1[1], coords1[0]])
+                marker1.addTo(map).bindPopup(route.stNam1);
+                marker2 = L.marker([coords2[1], coords2[0]])
+                marker2.addTo(map).bindPopup(route.stNam2);
+                polyline = L.polyline([[coords1[1], coords1[0]], [coords2[1], coords2[0]]], { color: 'blue' })
+                polyline.addTo(map);
+                this.mapElements.push(marker1,marker2,polyline);
+
+            });
+            clickedShortest = true
         });
-    });
+    }
+    else {
+        for (var i = 0; i < this.mapElements.length; i++) {
+            map.removeLayer(this.mapElements[i]);
+        }
+        clickedShortest = false
+
+    }
+
+
 }
