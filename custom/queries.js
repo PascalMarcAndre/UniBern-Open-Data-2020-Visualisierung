bc504ae63342b49e1e3e730d9c107370d979ee2a
saidalidev <said-ali1997@hotmail.com>
2020-05-07 18:39:58 +0200

    Add query for zoningplan stations

--- custom/queries.js
+++ custom/queries.js
@@ -194,6 +194,23 @@ function query_allZoningplans() {
     `
 }
 
+function query_ZoningPlanStations(zoningPlan) {
+    return `
+    PREFIX sc: <http://purl.org/science/owl/sciencecommons/>
+    PREFIX gtfs: <http://vocab.gtfs.org/terms#>
+    PREFIX schema: <http://schema.org/>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX otd: <http://lod.opentransportdata.swiss/vocab/> PREFIX dcterms: <http://purl.org/dc/terms/>
+    SELECT distinct ?Station ?Name ?Coord ?departureID
+    WHERE {
+    ?Kante a otd:Relation; otd:zoningPlan
+    <` + zoningPlan + `>; schema:departureStation ?Station .
+    ?Station rdfs:label ?Name ;
+    <http://www.opengis.net/ont/geosparql#hasGeometry>/<http://www.opengis.net/ont/geosparql#asWKT> ?Coord;              dcterms:identifier ?departureID .
+    } 
+    `
+}
+
 //TODO, manipulate query
 function query_allMonoDirectionalShortDistancesForStation() {
     return `
--- queries/query_overview.md
+++ queries/query_overview.md
@@ -139,6 +139,25 @@ WHERE {
 } 
 ````
 
+### Stations from a specific Zoning plan
+Returns all zoning plans
+
+````
+PREFIX sc: <http://purl.org/science/owl/sciencecommons/>
+PREFIX gtfs: <http://vocab.gtfs.org/terms#>
+PREFIX schema: <http://schema.org/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX otd: <http://lod.opentransportdata.swiss/vocab/> PREFIX dcterms: <http://purl.org/dc/terms/>
+SELECT distinct ?Station ?Name ?Coord ?departureID
+WHERE {
+?Kante a otd:Relation; otd:zoningPlan
+<http://lod.opentransportdata.swiss/zoningplan/zvv/zvv-abo-26amp-3b-billett>; schema:departureStation ?Station .
+?Station rdfs:label ?Name ;
+<http://www.opengis.net/ont/geosparql#hasGeometry>/<http://www.opengis.net/ont/geosparql#asWKT> ?Coord;              dcterms:identifier ?departureID .
+} 
+
+````
+
 ### All  monodirectional shortdistances
 Returns monodirectional shortdistances
 
